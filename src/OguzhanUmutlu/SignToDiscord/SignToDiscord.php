0baf081c79cb2148b96eb60b66de26ead263b088
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2021-08-07 20:51:58 +0300

    blocked pings

--- src/OguzhanUmutlu/SignToDiscord/SignToDiscord.php
+++ src/OguzhanUmutlu/SignToDiscord/SignToDiscord.php
@@ -19,7 +19,7 @@ class SignToDiscord extends PluginBase {
     public static function sendToWebhook(string $message) {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, self::getInstance()->getConfig()->getNested("webhook-url"));
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(["content" => $message." ** **", "username" => self::getInstance()->getConfig()->getNested("webhook-name")]));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(["content" => str_replace("@", "\\@", $message)." ** **", "username" => self::getInstance()->getConfig()->getNested("webhook-name")]));
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
